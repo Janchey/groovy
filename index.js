bf8723f3cec3ae5cbcadfdd3bdb2a44185eb3979
Janchey <dejan.jankov.20@gmail.com>
2018-03-01 16:41:02 +0100

    creating user authentication v1.1

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const config = require('./config/database');
 const path = require('path');
+const authenticate = require('./routes/authentication')(router);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
@@ -13,8 +16,11 @@ mongoose.connect(config.uri, (err) => {
         console.log('Connected to database: ' + config.db);
     }
 });
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/frontend/dist/'));
+app.use('/authentication', authenticate);
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/frontend/dist/index.html'));
 });
